ad6dc772d8e35d7327d49204e6b55d4f3e60cd4f
Rubén Ma <ruben9889maranon@gmail.com>
2025-11-03 09:43:22 +0100

    Vuelta el paso anterior que no habia guardado y no se sube por eclipse

--- src/main/java/org/cuatrovientos/dam/psp/tamagotchis/Tamagotchi.java
+++ src/main/java/org/cuatrovientos/dam/psp/tamagotchis/Tamagotchi.java
@@ -1,5 +1,9 @@
 package org.cuatrovientos.dam.psp.tamagotchis;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Representa un Tamagotchi.
  * Cada instancia está diseñada para correr en su propio hilo.
@@ -12,10 +16,24 @@ public class Tamagotchi implements Runnable {
     private volatile boolean alive;
     
     
-    public Tamagotchi(String name) {
-        this.name = name;
-        this.alive = true; // Nace vivo
-    }
+     //atriibutos de cocurrencia
+      private final AtomicInteger dirtiness; // la sucio de de tamagotchi 0-10
+     // 'BlockingQueue' es el "buzón" thread-safe para recibir órdenes.
+      private final BlockingQueue<Action> actionQueue;
+
+      // atributos de vida
+      private long startTime;      // Cuándo nació (para la muerte por edad)
+      private long lastDirtTick;   // Cuándo se ensució por última vez
+    
+    
+      public Tamagotchi(String name) {
+	    this.name = name;
+	    this.alive = true;
+	    
+	    // nuevos atribustos del tamagotchi
+	    this.dirtiness = new AtomicInteger(0);
+	    this.actionQueue = new LinkedBlockingQueue<>(); // Una cola enlazada, sin límite
+	}
 
     
     @Override
