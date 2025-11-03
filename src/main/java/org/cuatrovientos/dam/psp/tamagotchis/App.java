cfce441b996c3529555c342ac44b66d82826adf5
Rubén Ma <ruben9889maranon@gmail.com>
2025-11-03 08:46:23 +0100

    Proyecto tamagotchis hilos

---
+++ src/main/java/org/cuatrovientos/dam/psp/tamagotchis/App.java
@@ -0,0 +1,13 @@
+package org.cuatrovientos.dam.psp.tamagotchis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
